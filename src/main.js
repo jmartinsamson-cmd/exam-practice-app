186835f0765d2e7370cdb631aa66c41045fa7360
jmartinsamson-cmd <j.martinsamson@gmail.com>
2025-09-15 09:25:39 -0500

    Fix corrupted main.js file and finalize build configuration

--- src/main.js
+++ src/main.js
@@ -1,28 +1,3 @@
-import { StrictMode } from 'react'import { StrictMode } from 'react'import { StrictMode } from 'react'
-
-import { createRoot } from 'react-dom/client'
-
-import './index.css'import { createRoot } from 'react-dom/client'import { createRoot } from 'react-dom/client'
-
-import App from './App'
-
-import './index.css'import './index.css'
-
-createRoot(document.getElementById('root')).render(
-
-  <StrictMode>import App from './App'import App from './App'
-
-    <App />
-
-  </StrictMode>,
-
-)
-createRoot(document.getElementById('root')).render(createRoot(document.getElementById('root')).render(
-
-  <StrictMode>  <StrictMode>
-
-    <App />    <App />
-
-  </StrictMode>,  </StrictMode>,
-
-))
+// This is a JavaScript entry point that will be processed by Vite
+// It simply imports the main JSX file
+import './main.jsx';
